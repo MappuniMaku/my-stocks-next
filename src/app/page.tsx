6fabe8d8bd87999ce289e04f7357f2ab44557e0d
Grigory Telenok <telenok.grigory@gmail.com>
2024-05-04 22:37:45 +1000

    Test CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-semibold">MyStocks</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-semibold">MyStocks</h1>
+      <p className="mt-4 text-medium">Добро пожаловать в приложение MyStocks!</p>
+    </>
+  );
 }
